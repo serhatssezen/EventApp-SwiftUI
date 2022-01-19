5ceab668ec4af9eb3b07eb0a6e243c844a3303b6
Aydın Serhat SEZEN <aydinserhat.sezen@kocsistem.com.tr>
2022-01-19 22:51:58 +0300

    FirstCommit

---
+++ Event/Event/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Event
+//
+//  Created by Aydın Serhat Sezen on 19.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Event/Event/EventApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventApp.swift
+//  Event
+//
+//  Created by Aydın Serhat Sezen on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct EventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
